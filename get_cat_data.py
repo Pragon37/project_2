57bf250364c68479f5e24ad07a03951aee3438df
Pierre Ragon <pragon37@outlook.fr>
2021-07-08 10:40:58 +0200

    Old code left commented out

--- get_cat_data.py
+++ get_cat_data.py
@@ -27,6 +27,7 @@ print()
 book = {}
 SELECTOR = {}
 
+"""
 page = requests.get(SITEURL)
 
 if page.status_code != 200:
@@ -36,22 +37,20 @@ else:
     content = page.content
 
 soup = BeautifulSoup(content, 'html.parser')
+"""
 
 allCategories = []
 
 
 def get_category_url(inpt):
     "Return category url string from a category selector"
+    "and category from a category selector"
     return [str(inpt[0]['href']), str(inpt[0].text).strip()]
 
 
 CATEGORYSELECTOR = ('#default > div > div > div > aside > div.side_categories '
                     '> ul > li > ul > li:nth-child(0) > a')
 
-for i in range(1, 51):
-    categorySelector = CATEGORYSELECTOR.replace("0", str(i))
-    catData = soup.select(categorySelector)
-    allCategories.append((get_category_url(catData)))
 
 SELECTOR['title'] = ('head > title')
 
@@ -142,6 +141,7 @@ def get_bk_description(inpt):
             strip())
 
 
+"""
 def extract_book(fsoup, furl):
     "extract book data from soup:fsoup of the page url:furl"
     fbook = {}
@@ -169,6 +169,59 @@ def extract_book(fsoup, furl):
         elif key == 'description':
             fbook[key] = get_bk_description(data)
     return fbook
+"""
+
+
+"""
+def extract_book(fsoup, furl):
+    "extract book data from soup:fsoup of the page url:furl"
+    fbook = {}
+    data = fsoup.select(SELECTOR['title'])
+    fbook['title'] = get_bk_title(data)
+    data = fsoup.select(SELECTOR['category'])
+    fbook['category'] = get_bk_category(data)
+    data = fsoup.select(SELECTOR['upc'])
+    fbook['upc'] = get_bk_upc(data)
+    data = fsoup.select(SELECTOR['incltax'])
+    fbook['incltax'] = get_bk_incltax(data)
+    data = fsoup.select(SELECTOR['excltax'])
+    fbook['excltax'] = get_bk_excltax(data)
+    data = fsoup.select(SELECTOR['stock'])
+    fbook['stock'] = get_bk_stock(data)
+    data = fsoup.select(SELECTOR['rating'])
+    fbook['rating'] = get_bk_rating(data)
+    fbook['url'] = get_bk_url(furl)
+    data = fsoup.select(SELECTOR['img'])
+    fbook['img'] = get_bk_img(data)
+    data = fsoup.select(SELECTOR['description'])
+    fbook['description'] = get_bk_description(data)
+    return fbook
+"""
+
+
+def extract_book(fsoup, furl):
+    "extract book data from soup:fsoup of the page url:furl"
+    fbook = []
+    data = fsoup.select(SELECTOR['title'])
+    fbook.append(get_bk_title(data),)
+    data = fsoup.select(SELECTOR['category'])
+    fbook.append(get_bk_category(data),)
+    data = fsoup.select(SELECTOR['upc'])
+    fbook.append(get_bk_upc(data),)
+    data = fsoup.select(SELECTOR['incltax'])
+    fbook.append(get_bk_incltax(data),)
+    data = fsoup.select(SELECTOR['excltax'])
+    fbook.append(get_bk_excltax(data),)
+    data = fsoup.select(SELECTOR['stock'])
+    fbook.append(get_bk_stock(data),)
+    data = fsoup.select(SELECTOR['rating'])
+    fbook.append(get_bk_rating(data),)
+    fbook.append(get_bk_url(furl),)
+    data = fsoup.select(SELECTOR['img'])
+    fbook.append(get_bk_img(data),)
+    data = fsoup.select(SELECTOR['description'])
+    fbook.append(get_bk_description(data),)
+    return fbook
 
 
 def get_book_url(pageContent):
@@ -190,11 +243,50 @@ def get_book_url(pageContent):
             return urlList
             break
 
+"""
+List all categories.Not used so far. Will be when iterating through the whole
+site
+for i in range(1, 51):
+    categorySelector = CATEGORYSELECTOR.replace("0", str(i))
+    catData = soup.select(categorySelector)
+    allCategories.append((get_category_url(catData)))
+    print(allCategories[i-1])
+"""
+"""
+page = requests.get(SITEURL)
 
-page = requests.get("https://books.toscrape.com/catalogue/category/books/mystery_3/index.html")
-content = page.content
-bookList = get_book_url(content)
+if page.status_code != 200:
+    print("Error fetching page")
+    exit()
+else:
+    content = page.content
 
+soup = BeautifulSoup(content, 'html.parser')
+"""
+
+
+filename = 'fantasy.csv'
+CATBASEURL = "https://books.toscrape.com/catalogue/category/books/fantasy_19"
+
+
+def get_book_list(catBaseUrl):
+    i = 1
+    booklist = []
+    pageHtml = "/index.html"
+    while True:
+        page = requests.get(catBaseUrl + pageHtml)
+        if page.status_code != 200:
+            return booklist
+            print("category add-a-comment has: ", str(i-1), " pages")
+            break
+        else:
+            content = page.content
+            booklist += get_book_url(content)
+            i = i + 1
+            pageHtml = "/page-" + str(i) + ".html"
+
+
+bookList = get_book_list(CATBASEURL)
 
 rows = []
 
@@ -212,10 +304,11 @@ for i in bookList:
 
     soup = BeautifulSoup(content, 'html.parser')
     book = extract_book(soup, url)
-    for key in book:
-        record.append(book[key], )
-    rows.append(record)
+#    for key in book:
+#        record.append(book[key], )
+#    rows.append(record)
+    rows.append(book)
 
-recordWriter = csv.writer(open('book_cat.csv', 'w', newline=''))
+recordWriter = csv.writer(open(filename, 'w', newline=''))
 recordWriter.writerow(CSVHEADER)
 recordWriter.writerows(rows)
